d3478cd28a56b074d717c7e5c88d3237fc8a58f3
amilabbasov02 <amilabbas451.19@gmail.com>
2024-05-24 19:11:35 +0400

    new

--- js/main.js
+++ js/main.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
         // templateResult: formatState,
         // templateSelection: formatState
     });
+    $('.number_select').select2({
+        minimumResultsForSearch: Infinity
+    });
     $('.room_select').on('select2:open', function() {
         $('.select2-search__field').attr('placeholder', 'Axtar');
     });
@@ -25,24 +28,56 @@ $(document).ready(function() {
         dateFormat: "dd-mm-yy",
         buttonText: "Seçin"
     })
-    function selectOption(optionElement) {
-        var selectedSpan = $("#selectedOption");
-        var selectedValue = $("#selectedValue");
+    function selectOption1(optionElement) {
+        var selectedSpan = $("#selectedOption1");
+        var selectedValue = $("#selectedValue1");
         var selectedClass = $(optionElement).attr("class").split(" ")[1];
         var selectedText = $(optionElement).text();
         selectedSpan.attr("class", selectedClass);
         selectedSpan.text(selectedText);
         selectedSpan.css("background-color", $(optionElement).css("background-color"));
         selectedValue.val(selectedText).attr("class", selectedClass);
-        $("#listContainer").hide();
+        $("#listContainer1").hide();
     }
-    function toggleListVisibility() {
-        $("#listContainer").toggle();
+
+    function toggleListVisibility1() {
+        $("#listContainer1").toggle();
     }
-    $(".option").click(function() {
-        selectOption(this);
+
+    $("#listContainer1 .option1").click(function() {
+        selectOption1(this);
     });
-    $("#inputContainer").click(function() {
-        toggleListVisibility();
+
+    $("#inputContainer1").click(function() {
+        toggleListVisibility1();
     });
+
+    // İkinci etiket ve liste için
+    function selectOption2(optionElement) {
+        var selectedSpan = $("#selectedOption2");
+        var selectedValue = $("#selectedValue2");
+        var selectedClass = $(optionElement).attr("class").split(" ")[1];
+        var selectedText = $(optionElement).text();
+        selectedSpan.attr("class", selectedClass);
+        selectedSpan.text(selectedText);
+        selectedSpan.css("background-color", $(optionElement).css("background-color"));
+        selectedValue.val(selectedText).attr("class", selectedClass);
+        $("#listContainer2").hide();
+    }
+
+    function toggleListVisibility2() {
+        $("#listContainer2").toggle();
+    }
+
+    $("#listContainer2 .option2").click(function() {
+        selectOption2(this);
+    });
+
+    $("#inputContainer2").click(function() {
+        toggleListVisibility2();
+    });
+    $(".more_btn").click(function(){
+        $(".filter_inputs").toggleClass("active")
+        $(this).toggleClass("active")
+    })
 });
